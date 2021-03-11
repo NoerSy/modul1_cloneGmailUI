8e9718ac4f907c1c4e63d5b8709c5382fdf7ba63
Nur Syahfei <55045713+NoerSy@users.noreply.github.com>
2021-03-12 00:48:44 +0700

    Update DashboardPage.dart

--- lib/view/DashboardPage/DashboardPage.dart
+++ lib/view/DashboardPage/DashboardPage.dart
@@ -20,10 +20,10 @@ class _DasboardPageState extends State<DasboardPage> {
           bottom: PreferredSize(
             preferredSize: Size(
               0,
-              50,
+              15,
             ),
             child: Padding(
-              padding: const EdgeInsets.only(left: 14, right: 14),
+              padding: const EdgeInsets.only(left: 10, right: 10),
               child: Card(
                 color: Colors.amber,
                 elevation: 10,
@@ -31,6 +31,7 @@ class _DasboardPageState extends State<DasboardPage> {
                   borderRadius: BorderRadius.circular(5),
                 ),
                 child: Container(
+                  padding: const EdgeInsets.all(4),
                   alignment: Alignment.topCenter,
                   child: Row(
                     children: [
@@ -57,7 +58,10 @@ class _DasboardPageState extends State<DasboardPage> {
                         child: Container(
                           child: Center(
                             child: CircleAvatar(
-                              child: Text("Da"),
+                              radius: 24.0,
+                              backgroundImage: NetworkImage(
+                                "https://lh3.googleusercontent.com/fife/ABSRlIoIsW1qGZwowIikK7zSG95_--QfGweUqAginSm7Ho6R6_QRnC-tAd-W42hUfQ6cCJ4hngtSwKqvaEm1N4dKZRGlj-8dZnDsFfrb7J1gYogWTvCThPduM_OMQK6jE_kk0jwrTRpWa0FGoWiXB-NLJtC5ull31rWeklHxKVPcjgUX-ePFFpeq7W0wf6ZwjEt0RV37M1OJNLIvaNqgClQayaVOVSSDMxkCrzk1twLFVIaDLrHsoH9y1Q9CWkcs--zOj6fppCOPfM21M7t5sJ9kVns0KvqYHKso_bb8p-a97rKWNFVKimNqcsv5qhc48F5uSopufTVPlJkYeZtd4VunXH6fGHjCHrBnrStcrW_0Z3ibWMwsITC5ij548PMEfqRvuWWTV9z12Gs1tfPLLtAQzC49SbRXR0QoEKHJ3y9EIqum01ND50jeK3sKKJorq34r6RRt8XNiHjDEup0uXlyy3HUf5-tDJdxnwnLY8RTacM6afplcIjaL0IqSVtRggc6S6oipLdBdpquaNSDVsbx2eUPUuVE59DRg8wbDQ8rP-TDf-7NR1_bMxNiacXQPaW_S1J8hg8CFLylWVSeb8q_tVg13fjclDV2EhFdGD0o47PspmOGdcpbmyUdOVEdTlgDjTVEJw1miEDxCqNVmpq8g8cCxJx20A-yam7cJvYhEn9hAdfAY_9-tP8xNf0SCep8jleRFdizHOB5oEfMaOINJBGIJ-vW8LVH5-WkWrynxZxo5PBM=s83-c",
+                              ),
                             ),
                           ),
                         ),
